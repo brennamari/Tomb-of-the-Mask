1be8ef37798a547cf8c99f76dc29e65445cd5b55
KARLA JULIANE SANTOS MEIRA <j.karla@academico.ifrn.edu.br>
2023-12-29 07:52:31 -0300

    MOVIMENTAÇÃO BÁSICA

--- My project/Assets/Scripts/Movement.cs
+++ My project/Assets/Scripts/Movement.cs
@@ -15,9 +15,6 @@ public class Movement : MonoBehaviour
     private Rigidbody2D rb;
 
     private Direction movingDir;
-
-    [SerializeField] bool movingHorizontally = false, canCheck = true;
-    [SerializeField] LayerMask obstacleMask;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,80 +24,50 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (movingHorizontally)
+        if (Input.GetAxisRaw("Horizontal") != 0)
         {
-            if(Physics2D.Raycast(transform.position, Vector2.left, .6f, obstacleMask) || Physics2D.Raycast(transform.position, Vector2.right, .6f, obstacleMask))
+            rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+
+            if (Input.GetAxisRaw("Horizontal") > 0)
             {
-                canCheck = true;
+                movingDir = Direction.East;
             }
             else
             {
-                canCheck = false;
-            }
-            
-            if(Physics2D.Raycast(transform.position, Vector2.up, .6f, obstacleMask) || Physics2D.Raycast(transform.position, Vector2.down, .6f, obstacleMask))
-            {
-                canCheck = true;
-            }
-            else
-            {
-                canCheck = false;
+                movingDir = Direction.West;
             }
         }
-
-        if (canCheck)
+        else if (Input.GetAxisRaw("Vertical") != 0)
         {
-            if (Input.GetAxisRaw("Horizontal") != 0)
-            {
-                rb.constraints = RigidbodyConstraints2D.FreezePositionY;
-                movingHorizontally = true;
-
-                if (Input.GetAxisRaw("Horizontal") > 0)
-                {
-                    movingDir = Direction.East;
-                }
-                else
-
-                {
-                    movingDir = Direction.West;
-                }
-            }
-            else if(Input.GetAxisRaw("Vertical") != 0)
-
-            {
-                rb.constraints = RigidbodyConstraints2D.FreezePositionX;
-                movingHorizontally = false;
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX;
             
-                if (Input.GetAxisRaw("Vertical") > 0)
-                {
-                    movingDir = Direction.North;
-                }
-                else
-
-                {
-                    movingDir = Direction.South;
-                }
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                movingDir = Direction.North;
+            }
+            else
+            {
+                movingDir = Direction.South;
             }
         }
     }
 
-        private void FixedUpdate()
+    private void FixedUpdate()
     {
         switch (movingDir)
         {
-            case Direction.North:
-                rb.velocity = new Vector2(0, speed * Time.fixedDeltaTime);   
-                break;
-            case Direction.South:
-                rb.velocity = new Vector2(0, -speed * Time.fixedDeltaTime);   
-                break;
-            case Direction.East:
-                rb.velocity = new Vector2(speed * Time.fixedDeltaTime, 0);   
-                break;
-            case Direction.West:
-                rb.velocity = new Vector2(-speed * Time.fixedDeltaTime, 0);
-                break;
-            
+           case Direction.North:
+               rb.velocity = new Vector2(0, speed * Time.fixedDeltaTime);
+               break;
+           case Direction.South:
+               rb.velocity = new Vector2(0, -speed * Time.fixedDeltaTime);
+               break;
+           case Direction.East:
+               rb.velocity = new Vector2( speed * Time.fixedDeltaTime, 0);
+               break;
+           case Direction.West:
+               rb.velocity = new Vector2( -speed * Time.fixedDeltaTime, 0);
+               break;
         }
     }
 }
